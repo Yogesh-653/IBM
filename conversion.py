2c66a120a591ba35de25509a38888af06631460e
yogesh653 <60870157+yogesh653@users.noreply.github.com>
2020-02-14 01:10:01 -0800

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,11 @@
+s = "10010"
+
+# printing string converting to int base 2
+c = int(s, 2)
+print("After converting to integer base 2 : ", end="")
+print(c)
+
+# printing string converting to float
+e = float(s)
+print("After converting to float : ", end="")
+print(e)
